ca17980412d3ed17492d1886fe8a565eba6ff688
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2023-01-20 18:26:31 +0530

    Fetch data from json

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,29 @@
 import './App.css'
 import Blog from "./Blog.json"
 export default function App() {
+  const students=[
+    {Student:"Arnab",id:1},
+    {Student:"Sameer",id:2},
+    {Student:"Prakash",id:3}
+  ]
   return (
     <main>
       React ⚛️ + Vite ⚡ + Replit 🌀
       {
         Blog && Blog.map((result)=>{
           return (
-            <div className='box'><li key={result.id}>{result.Name}</li>
+            <div>
+              {students && students.map((stdata)=>(
+              <li key={stdata}>{stdata.Student}</li>
+              ))}
+
+              
+              <li key={result.id}>{result.Name}</li>
              {result.tech && result.tech.map((data)=>{
               return (<strong key={result.id}>{data.Course}</strong>)
              })}
             </div>
-           
+            
           )
         })
       }
